9db7cf65f36574748476a2cba006f14f3437c365
dwilliams304 <d.williams0434@gmail.com>
2024-02-23 09:49:14 -0700

    Filter menu setup

---
+++ frontend/src/components/layout/FilterMenu.tsx
@@ -0,0 +1,35 @@
+export default function FilterMenu() {
+    return(
+        <div>
+            <h2>Filters</h2>
+            <div>
+                <select id="project-name" name="project-name">
+                    <option value="Project 1">Project 1</option>
+                    <option value="Project 2">Project 2</option>
+                    <option value="Project 3">Project 3</option>
+                </select>
+
+                
+                <select id="ticket-severity" name="ticket-severity">
+                    <option value="Option 1">Option 1</option>
+                    <option value="Option 2">Option 2</option>
+                    <option value="Option 3">Option 3</option>
+                </select>
+
+                <select id="ticket-status" name="ticket-status">
+                    <option value="Option 1">Option 1</option>
+                    <option value="Option 2">Option 2</option>
+                    <option value="Option 3">Option 3</option>
+                </select>
+
+                <select id="ticket-tags" name="ticket-tags">
+                    <option value="Option 1">Option 1</option>
+                    <option value="Option 2">Option 2</option>
+                    <option value="Option 3">Option 3</option>
+                </select>
+
+
+            </div>
+        </div>
+    )
+}
