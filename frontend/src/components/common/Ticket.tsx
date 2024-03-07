af0778513dc0c0e64f226cd05b84f58ea180fbd5
dwilliams304 <d.williams0434@gmail.com>
2024-03-07 16:45:49 -0700

    wip

--- frontend/src/components/common/Ticket.tsx
+++ frontend/src/components/common/Ticket.tsx
@@ -1,7 +1,25 @@
 export default function Ticket(){
     return(
-        <div>
-            
+        <div className="ticket">
+            <div>
+                <p>ffff-00f</p>
+            </div>
+            <div>
+                <p>Losing data</p>
+            </div>
+            <div>
+                <p>Low</p>
+            </div>
+            <div>
+                <span>Sarah</span>
+                <span>Robert</span>
+            </div>
+            <div>
+                <p>Data</p>
+            </div>
+            <div>
+                <p>31d</p>
+            </div>
         </div>
     )
 }
