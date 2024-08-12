2685b7e9c8cf97f8a3d0dcfa3f2b4f053f9421ca
dwilliams304 <d.williams0434@gmail.com>
2024-08-12 11:01:58 -0400

    wip

--- packages/frontend/src/components/Ticket.tsx
+++ packages/frontend/src/components/Ticket.tsx
@@ -1,6 +1,11 @@
 export default function Ticket(){
     return(
-        <div>
+        <div className="w-full h-16 border-b-slate-200 border-b-2 
+        cursor-pointer hover:shadow-md grid grid-cols-5 p-4 items-center">
+            <h4>Ticket</h4>
+            <h4>Ticket</h4>
+            <h4>Ticket</h4>
+            <h4>Ticket</h4>
             <h4>Ticket</h4>
         </div>
     )
--- packages/frontend/src/pages/TicketList.tsx
+++ packages/frontend/src/pages/TicketList.tsx
@@ -3,8 +3,12 @@ import { Ticket } from "../components"
 export default function TicketList(){
     return(
         <div className="w-full h-full flex flex-col py-12 px-6">
-            <div className="text-center">
-                <h2>Bug List</h2>
+            <div className="grid grid-cols-5 w-3/4 mx-auto bg-slate-300 h-14 p-4 items-center">
+                <h2>Days Open</h2>
+                <h2>Description</h2>
+                <h2>Severity</h2>
+                <h2>Assigned To</h2>
+                <h2>Tags</h2>
             </div>
 
             <div className="border border-slate-400 w-3/4 justify-center mx-auto space-y-2">
