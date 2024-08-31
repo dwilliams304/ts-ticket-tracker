7e6b1a0122a4f05ac4ade60d726ad949a879c9de
dwilliams304 <d.williams0434@gmail.com>
2024-08-31 12:51:55 -0400

    match navbar button color to addticketbutton color

--- packages/frontend/src/layout/NavBar.tsx
+++ packages/frontend/src/layout/NavBar.tsx
@@ -1,6 +1,6 @@
 export default function NavBar(){
     return(
-        <nav className="flex w-full justify-between border-b-purple-800 border p-4">
+        <nav className="flex w-full justify-between border-b-purple-700 border p-4">
             {/* LEFT SIDE ELEMENTS */}
             <div className="w-1/3">
                 <h2>TypeScript Bug Tracker</h2>
@@ -13,10 +13,10 @@ export default function NavBar(){
 
             {/* RIGHT SIDE ELEMENTS */}
             <div className="w-1/3 flex flex-grow justify-end">
-                <button className="px-4 mx-2 bg-purple-800 text-white rounded-xl">
+                <button className="px-6 py-1 mx-2 border bg-purple-700 text-white rounded-xl">
                     Log In
                 </button>
-                <button className="px-4 mx-2 bg-purple-800 text-white rounded-xl">
+                <button className="px-6 py-1 mx-2 border bg-purple-700 text-white rounded-xl">
                     Support
                 </button>
             </div>
