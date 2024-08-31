a0233908cfff13cf869dd6dce69d7fb75c531060
dwilliams304 <d.williams0434@gmail.com>
2024-08-31 12:49:43 -0400

    create ticket button styling, change ticketdata.tsx to .ts

--- packages/frontend/src/components/AddTicketButton.tsx
+++ packages/frontend/src/components/AddTicketButton.tsx
@@ -1,6 +1,7 @@
 export default function AddTicketButton() {
     return(
-        <button className="sticky bottom-4 mx-auto rounded-xl border px-4 py-1">
+        <button className="sticky bottom-4 my-4 mx-auto border rounded-xl px-4 py-1 bg-violet-700 text-white
+        transition-all duration-300 ease-in-out hover:shadow-lg hover:text-violet-700 hover:bg-white">
             Create Ticket
         </button>
     )
