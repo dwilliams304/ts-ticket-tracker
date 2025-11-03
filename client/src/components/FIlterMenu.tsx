1fdefbdea5edb757230ef5c1c5e22c43928b5c3e
dwilliams304 <d.williams0434@gmail.com>
2025-11-02 22:22:43 -0500

    general styling, remove some items from filtermenu

--- client/src/components/FIlterMenu.tsx
+++ client/src/components/FIlterMenu.tsx
@@ -3,42 +3,25 @@ export default function FilterMenu(){
         <div className="filter-menu">
             <h2>Filters</h2>
             <div>
-                <select name="tags">
-                    <option>Option 1</option>
-                    <option>Option 2</option>
-                    <option>Option 3</option>
-                    <option>Option 4</option>
-                </select>
+                <div className="filter-option">
+                    <p>Severity</p>
+                    <select name="tags">
+                        <option>Option 1</option>
+                        <option>Option 2</option>
+                        <option>Option 3</option>
+                        <option>Option 4</option>
+                    </select>
+                </div>
                 
-                <select name="security">
-                    <option>Option 1</option>
-                    <option>Option 2</option>
-                    <option>Option 3</option>
-                    <option>Option 4</option>
-                </select>
-
-                <select>
-                    <option>Option 1</option>
-                    <option>Option 2</option>
-                    <option>Option 3</option>
-                    <option>Option 4</option>
-                </select>
-
-                <select>
-                    <option>Option 1</option>
-                    <option>Option 2</option>
-                    <option>Option 3</option>
-                    <option>Option 4</option>
-                </select>
-
-                <select>
-                    <option>Option 1</option>
-                    <option>Option 2</option>
-                    <option>Option 3</option>
-                    <option>Option 4</option>
-                </select>
-
-
+                <div className="filter-option">
+                    <p>Tags</p>
+                    <select name="tags">
+                        <option>Option 1</option>
+                        <option>Option 2</option>
+                        <option>Option 3</option>
+                        <option>Option 4</option>
+                    </select>
+                </div>
             </div>
         </div>
     )
